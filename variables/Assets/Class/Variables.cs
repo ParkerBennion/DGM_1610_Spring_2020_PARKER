1840278031738d0d62e46b69f462cacae4e2a821
Parker <parkerbennion@gmail.com>
2020-01-13 19:00:28 -0700

    added variable C# and unity

---
+++ variables/Assets/Class/Variables.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour
+{
+    private int bowl;
+    private int box;
+    // only round items fit in a bowl and only square ones in a box (;
+    //variables are most commonly stated before the start function... but can be placed and used whithin them.
+    private int tupper = 2;
+    // int is an intager (whole number)
+    // ints can be set to a value or left undefined and defined later;
+    
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
