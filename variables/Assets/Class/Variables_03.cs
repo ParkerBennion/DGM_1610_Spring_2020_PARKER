d4bed3f859d3b2548d3fd489271bec17cb467a25
Parker <parkerbennion@gmail.com>
2020-01-25 23:17:57 -0700

    attmpted to do a double mode

---
+++ variables/Assets/Class/Variables_03.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables_03 : MonoBehaviour
+{
+    public int apple = 1, orange = 5, blueberry = 10;
+
+    void OnMouseDown()
+    {
+        Gooseberry();
+    }
+
+    public void Gooseberry()
+    {
+        frutibowl = (apple + orange + blueberry);
+        
+    }
+
+    public void OnMouseUp()
+    {
+        frutibowl = apple + orange + blueberry;
+        public 
+             string = frutibowl;
+    }
+}
