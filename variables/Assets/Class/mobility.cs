9c3607e3b73e430da78fb188be52fa73c83a8a1e
parker <parkerbennion@gmail.com>
2020-01-27 17:14:03 -0700

    fixed movement script and added triggers

--- variables/Assets/Class/mobility.cs
+++ variables/Assets/Class/mobility.cs
@@ -5,5 +5,32 @@ using UnityEngine;
 
 public class mobility : MonoBehaviour
 {
+   public float speed, verticalInput;
+
+   void Update()
+   {
+      transform.Translate(-.2f,0,0);
+   }
+
+   private void OnCollisionEnter(Collision other)
+   {
+      if (other.gameObject.CompareTag("Floor"))
+      {
+         Debug.Log("ouch!!!!");
+      }
+      else if (other.gameObject.CompareTag("Object"))
+      {
+         Debug.Log("I've been stopped");
+      }
+      else
+      {
+         Debug.Log("Its quiet");
+      }
+   }
+
+   private void OnTriggerEnter(Collider other)
+   {
+      Debug.Log("You are in the dangerzone!");
+   }
    
 }
