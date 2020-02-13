852a00021dc8c6ee6904584e981d805283110f24
parker <parkerbennion@gmail.com>
2020-02-13 15:18:47 -0700

    troubleshoot with the shoot command

--- variables/Assets/Class/GunMech.cs
+++ variables/Assets/Class/GunMech.cs
@@ -6,11 +6,13 @@ public class GunMech : MonoBehaviour
 {
     public GameObject rubberBulletPrefab;
     public GameObject mag;
+    public GameObject gunCam;
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            Instantiate(rubberBulletPrefab, mag.transform.position, mag.transform.rotation);
+            Instantiate(rubberBulletPrefab, mag.transform.position, gunCam.transform.rotation);
+            Debug.Log(gunCam.transform.rotation);
         }
     }
 }
--- variables/Assets/Class/Velocity.cs
+++ variables/Assets/Class/Velocity.cs
@@ -6,17 +6,9 @@ public class Velocity : MonoBehaviour
 {
     public float speed = 50;
     public int goodbye = 3;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        transform.Translate(  transform.forward * Time.deltaTime * speed);
+        transform.Translate(  -transform.right * Time.deltaTime * speed);
         Destroy(gameObject, goodbye);
     }
 }
