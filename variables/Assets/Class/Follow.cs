d5041c3eae4145103836d9fe36e05f7af67b605e
parker <parkerbennion@gmail.com>
2020-02-19 17:33:28 -0700

    Added enemy spawn

---
+++ variables/Assets/Class/Follow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+    public GameObject player;
+    public Vector3 target;
+    public int moveSpeed = 5;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        player = GameObject.Find("Sphere");
+        target = player.transform.position;
+        transform.LookAt(target);
+        transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+    }
+}
--- variables/Assets/Class/SpawnManager.cs
+++ variables/Assets/Class/SpawnManager.cs
@@ -14,9 +14,10 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        int enemyIndex = Random.Range(0, enemyPrefabs.Length);
         if (Input.GetKeyDown(KeyCode.E))
         {
-            Instantiate(enemyPrefabs[enemyIndex], new Vector3(0, 0, 0), enemyPrefabs[enemyIndex].transform.rotation);
+            Instantiate(enemyPrefabs[enemyIndex], new Vector3(15, 2, 0), enemyPrefabs[enemyIndex].transform.rotation);
         }
 
     }
