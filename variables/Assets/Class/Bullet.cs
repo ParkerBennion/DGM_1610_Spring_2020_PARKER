555787ae71f8ed083a9039f74e1a9945760409f6
parker <parkerbennion@gmail.com>
2020-02-26 17:14:07 -0700

    adding health to characters

---
+++ variables/Assets/Class/Bullet.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public int damage = 1;
+    public int time = 5;
+    
+    void Start()
+    {
+        StartCoroutine(DestroyBullet());
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            var hit = other.gameObject;
+            var health = hit.GetComponent<EnemyHealth>();
+
+            if (health != null)
+            {
+                health.TakeDameage(damage);
+            }
+            Debug.Log("you hit me for "+damage+" damage i have "+ health +"left");
+        }
+    }
+
+    IEnumerator DestroyBullet()
+    {
+        yield return new WaitForSeconds(time);
+        Destroy(gameObject);
+    }
+}
