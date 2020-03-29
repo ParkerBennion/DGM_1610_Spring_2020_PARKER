f1ea8b1d9b3a685c0b6be600256054a945cc5c58
Parker <parkerbennion@gmail.com>
2020-03-29 15:26:02 -0600

    improved enum process

--- variables/Assets/Class/Enums.cs
+++ variables/Assets/Class/Enums.cs
@@ -5,20 +5,39 @@ using UnityEngine;
 
 public class Enums : MonoBehaviour
 {
-    
-    enum Days{Sun,Mon,Tues,Wed,Thurs,Fri,Sat};
 
-    private Days currentDay;
-        private void Start()
-        {
-            //currentDay = Days.sat;
-        }
+    public enum ColorSelector
+    {
+        Red,
+        Blue,
+        Green,
+        Yellow
+    }
 
-        private void Update()
+    public ColorSelector CurrentColor;
+
+    private void Start()
+    {
+        CurrentColor = ColorSelector.Blue;
+        switch (CurrentColor)
         {
-            if (currentDay == Days.Fri)
-            {
-                print("is break time");
-            }
+            case ColorSelector.Blue:
+                Debug.Log("you selected blue");
+                if (Time.time > 5)
+                {
+                    CurrentColor = ColorSelector.Yellow;
+                }
+                // this does not change in start because time is at 0 when the game starts and code block ends process
+                break;
+            case ColorSelector.Green:
+                Debug.Log("you selected green");
+                break;
+            case ColorSelector.Red:
+                Debug.Log("you selected red");
+                break;
+            case ColorSelector.Yellow:
+                Debug.Log("you selected yellow");
+                break;
         }
+    }
 }
