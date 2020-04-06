aa7e1bf0c9e5fdf84ee222a0adeaeac3f1712b84
Parker <parkerbennion@gmail.com>
2020-04-06 17:15:42 -0600

    nav mesh

---
+++ variables/Assets/Class/Wanderer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using Random = System.Random;
+
+public class Wanderer : MonoBehaviour
+{
+   public float wanderRadius;
+   public float wanderTimer;
+
+   private Transform target;
+   private NavMeshAgent agent;
+   private float timer;
+
+   private void OnEnable()
+   {
+      agent = GetComponent<NavMeshAgent>();
+      timer = wanderTimer;
+   }
+
+   private void Update()
+   {
+      timer += Time.deltaTime;
+      if (timer>=wanderTimer)
+      {
+         Vector3 newPos = RandomNavSphere(transform.position, wanderRadius, -1);
+         agent.SetDestination(newPos);
+         timer = 0;
+      }
+   }
+
+   public static Vector3 RandomNavSphere(Vector3 origin, float dist, int layermask)
+   {
+      Vector3 randDirection = Random.insideUnitSphere * dist; //whats wrong with this?
+      randDirection += origin;
+      NavMeshHit navHit;
+      NavMesh.SamplePosition(randDirection, out navHit, dist, layermask);
+      return navHit.position;
+   }
+}
