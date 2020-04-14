eeddb870b67e38e809e0d548b5fad2f56852f39d
Parker <parkerbennion@gmail.com>
2020-04-13 22:39:39 -0600

    Added health script

---
+++ Factory Dash/Assets/Scripts/BulletDamage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletDamage : MonoBehaviour
+{
+    public int damage = 2;
+    public int time = 3;
+
+    private void Start()
+    {
+        Destroy(gameObject, time);
+        //this line of code and DestroyBullet in this scenario serve the same function. having both is redundant
+        StartCoroutine(DestroyBullet());//this runs coroutene below.
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        var health = other.gameObject.GetComponent<EnemyHealth>();
+
+        if (health != null)
+        {
+            health.TakeDamage(damage);
+        }
+        // for this to funcion a take damage and an enemyHealth coroutene and data need to be made.
+    }
+
+    IEnumerator DestroyBullet()
+    {
+        yield return  new WaitForSeconds(time);
+        
+    }
+}
---
+++ Factory Dash/Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int currentHealth;
+    public int maxHealth = 3;
+    //publics can be changed in inspector. privates cannot
+
+    public Transform spawnPoint;
+    public int points;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+        //short hand for taking current health minus what is equal to the amound taken away;
+
+        if (currentHealth <= 0)
+        {
+            currentHealth = 0;
+            Debug.Log("ouch you lose");
+            
+            
+        }
+    }
+    
+}
